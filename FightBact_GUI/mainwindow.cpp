16270b08508f26742c6b0e42b765a513199b1805
chuntze510 <a91178xx@gmail.com>
2019-06-30 23:47:48 +0800

    upload

--- FightBact_GUI/mainwindow.cpp
+++ FightBact_GUI/mainwindow.cpp
@@ -16,10 +16,10 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
   //connect(serial, static_cast<void (QSerialPort::*)(QSerialPort::SerialPortError)>(&QSerialPort::error)this, &MainWindow::handleError);
-
-   QObject::connect(serial, &QSerialPort::readyRead, this, &MainWindow::readData);
-  //connect(console, &Console::getData, this, &MainWindow::writeData);
    serial = new QSerialPort(this);
+   connect(serial, &QSerialPort::readyRead, this, &MainWindow::on_X_L_clicked);
+   connect(serial, &QSerialPort::readyRead, this, &MainWindow::onbtn2);
+
    ui->setupUi(this);
    connect(ui->X_R, SIGNAL(clicked(bool)), this, SLOT(onbtn2()));
    connect(ui->connect, SIGNAL(clicked(bool)), this, SLOT(openSerialPort()));
@@ -34,26 +34,32 @@ MainWindow::~MainWindow()
 void MainWindow::on_X_L_clicked()
 {
     qDebug()<<"you click XL";
+    QByteArray XL(":U1000");
+    XL=serial->readAll();
+    serial->write(XL);
 
 }
 
 void MainWindow::onbtn2()
 {
     qDebug()<<"you click XR";
+    QByteArray XR(":U-1000");
+    XR=serial->readAll();
+    serial->write(XR);
+
 }
 
 
 void MainWindow::openSerialPort()
 {
-    QSerialPort serial;
-    serial.setPortName("COM3");
-    serial.setBaudRate(QSerialPort::Baud9600);
-    serial.setDataBits(QSerialPort::Data8);
-    serial.setParity(QSerialPort::NoParity);
-    serial.setStopBits(QSerialPort::OneStop);
-    serial.setFlowControl(QSerialPort::NoFlowControl);
-    serial.open(QIODevice::ReadWrite);
-    if(!serial.open(QIODevice::ReadWrite))
+    serial->setPortName("COM3");
+    serial->setBaudRate(QSerialPort::Baud9600);
+    serial->setDataBits(QSerialPort::Data8);
+    serial->setParity(QSerialPort::NoParity);
+    serial->setStopBits(QSerialPort::OneStop);
+    serial->setFlowControl(QSerialPort::NoFlowControl);
+    serial->open(QIODevice::ReadWrite);
+    if(!serial->open(QIODevice::ReadWrite))
           {
               qDebug()<<"can't not open";
               QMessageBox::about(NULL, "notice", "can't not open");
@@ -72,7 +78,7 @@ void MainWindow::readData()
 
 void MainWindow::on_send_clicked()
 {
-    QByteArray data = ui->sendTextEdit->toPlainText().toUtf8();
+    QByteArray data = ui->sendTextEdit->toPlainText().toLocal8Bit();
     serial->write(data);
 }
 
